69ce27656cbbef2103a168bda91fe1247f7c08b9
Kevin Fabio Ramos L <kramosl@unal.edu.co>
2023-06-11 20:03:09 -0500

    :sparkles: popup login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './styles.css';
+import './styles.css'; // Importa tu archivo CSS personalizado aquí
 import axios from 'axios';
+import Modal from 'react-modal';
 
 function Login() {
   const [emailOrPhone, setEmailOrPhone] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [modalMessage, setModalMessage] = useState('');
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -22,13 +26,23 @@ function Login() {
       if (response.status === 200) {
         // guardar el token en el local storage
         localStorage.setItem('token', response.data.token);
+        //get token from local storage
         // redireccionar a la ruta /appMain
         window.location.href = '/appMain';
       }
     } catch (error) {
-      console.log(error.response.data);
-      console.error(error);
-      // errores de conexión con el servidor
+      console.log(error.response.status);
+      if (error.response.status === 401) {
+        setModalMessage('Credenciales incorrectas');
+      }
+      else if (error.response.status === 404) {
+        setModalMessage('Usuario no encontrado');
+      }
+      else{
+        setModalMessage('Error del servidor');
+      }
+
+      setModalIsOpen(true);
     }
   };
 
@@ -70,6 +84,11 @@ function Login() {
       <footer>
         <p>&copy; 2023 Comic Webpage. All rights reserved.</p>
       </footer>
+
+      <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)} className="custom-modal"> {/* Agrega la clase "custom-modal" al modal */}
+        <div>{modalMessage}</div>
+        <button onClick={() => setModalIsOpen(false)}>Cerrar</button>
+      </Modal>
     </div>
   );
 }
